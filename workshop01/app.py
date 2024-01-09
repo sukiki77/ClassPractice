9b75cb1418713b710938d059cf33be43155254cc
QI SHEN <qishenki@163.com>
2024-01-10 02:37:49 +0800

    First try - uploading code

---
+++ workshop01/app.py
@@ -0,0 +1,22 @@
+class Weapon:
+    def __init__(self, name, weapon_type, level):
+        self.name = name
+        self.weapon_type = weapon_type
+        self.level = 1
+
+    def display(self):
+        print(self.name + ' ' + self.weapon_type + ' ' + str(self.level))
+
+    def upgrade(self):
+        self.level += 1
+
+def run():
+    weapon1 = Weapon('Long Sword', 'Sword',1)
+    weapon1.display()
+    weapon2 = Weapon('Short Spear', 'Spear',1)
+    weapon2.display()
+    weapon1.upgrade()
+    weapon2.upgrade()
+    weapon1.display()
+    weapon2.display()
+
